72302948700ae509d7353a73216deda06eeda5f7
Axel ESSONO <naxelcdric@gmail>
2020-07-21 02:51:30 +0200

    Created function to create new city in Sqlitedatabase

--- app/src/main/java/com/obiangetfils/weatherapp/database/Database.kt
+++ app/src/main/java/com/obiangetfils/weatherapp/database/Database.kt
@@ -1,8 +1,10 @@
 package com.obiangetfils.weatherapp.database
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.obiangetfils.weatherapp.city.City
 
 private const val DATABASE_NAME = "weather.db"
 private const val DATABASE_VERSION = 1
@@ -19,13 +21,22 @@ private const val CITY_TABLE_CREATE = """
 )
 """
 
-class Database(context : Context) :
-   SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+class Database(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase?) {
         db?.execSQL(CITY_TABLE_CREATE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
     }
+
+    fun createCity(city: City): Boolean {
+        val values = ContentValues()
+        values.put(CITY_KEY_ID, CITY_KEY_NAME)
+        val insert = writableDatabase.insert(CITY_TABLE_NAME, null, values)
+        city.cityId = insert
+
+        return insert > 0
+    }
+
 }
